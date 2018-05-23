26b1276202fff8650c2744e2b45ff07867c1ff19
Maria Hudyakova <manyasha1975@rambler.ru>
2018-05-23 23:36:32 +0500

    Вычислени расстояния через метод (не закончено) -

--- sandbox/src/main/java/ru/mytest/functions/Point.java
+++ sandbox/src/main/java/ru/mytest/functions/Point.java
@@ -4,4 +4,8 @@ public class Point {
 
     public double x;
     public double y;
+
+    public double distance() {
+        return Math.sqrt((p2.x-this.x)*(p2.x-this.x) + (p2.y-this.y)*(p2.y-this.y));
+    }
 }
