db04fdeb7561e618aaf33083f5e0ca1a4227b29d
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-09 22:52:40 +0500

    Реализовано вычисление через фунцию

--- sandbox/src/main/java/ru/mytest/functions/Point.java
+++ sandbox/src/main/java/ru/mytest/functions/Point.java
@@ -5,7 +5,4 @@ public class Point {
     public double x;
     public double y;
 
-    public double distance() {
-        return Math.sqrt((p2.x-this.x)*(p2.x-this.x) + (p2.y-this.y)*(p2.y-this.y));
-    }
 }
