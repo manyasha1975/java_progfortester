cb1d7fa7990c1fd8a323ad688064e70cef177dce
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-15 22:47:00 +0500

    Реализовано вычисление через метод

--- sandbox/src/main/java/ru/mytest/functions/CalculationMethod.java
+++ sandbox/src/main/java/ru/mytest/functions/CalculationMethod.java
@@ -1,15 +1,9 @@
 package ru.mytest.functions;
 
-public class Calculation {
+public class CalculationMethod {
     public static void main (String[] args) {
- /*       Point p1 = new Point();
-        p1.x = 5.0;
-        p1.y = 5.0;
-        Point p2 = new Point();
-        p2.x = 2.0;
-        p2.y = 1.0;*/
-        Point p1 = new Point(7.0, 5.0);
-        Point p2 = new Point(2.0, 3.0);
+        Point p1 = new Point(5.0, 5.0);
+        Point p2 = new Point(2.0, 1.0);
         System.out.println("Расстояние между точками с координатами " + "(" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно = " + distance(p1, p2));
 
     }
