0615a1a0779c10df06976ee5369218a1f725392a
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-16 12:31:10 +0500

    Вычисление расстояния при помощи метода в классе Point (final)

--- sandbox/src/main/java/ru/mytest/functions/CalculationMethod.java
+++ sandbox/src/main/java/ru/mytest/functions/CalculationMethod.java
@@ -2,13 +2,9 @@ package ru.mytest.functions;
 
 public class CalculationMethod {
     public static void main (String[] args) {
-        Point p1 = new Point(5.0, 5.0);
-        Point p2 = new Point(2.0, 1.0);
-        System.out.println("Расстояние между точками с координатами " + "(" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно = " + distance(p1, p2));
+        Point p = new Point(10.0, 5.0, 25.0, 1.0);
+        System.out.println("Расстояние между точками с координатами " + "(" + p.x1 + "," + p.y1 + ") и (" + p.x2 + "," + p.y2 + ") равно = " + p.distance());
 
     }
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
-    }
 }
