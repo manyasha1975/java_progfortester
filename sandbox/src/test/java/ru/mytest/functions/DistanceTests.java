a9832c985fb0a96252d7f61746141336af135c4d
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-17 16:27:17 +0500

    ПДТ-57 Подключен тестовый фреймворк TestNG и реализованы тесты для класса Point

--- sandbox/src/test/java/ru/mytest/functions/DistanceTests.java
+++ sandbox/src/test/java/ru/mytest/functions/DistanceTests.java
@@ -8,23 +8,27 @@ public class DistanceTests {
 
     @Test
     public void DistTest1() {
-        Point d = new Point (5.0, 5.0, 2.0, 1.0);
-        Assert.assertEquals(d.distance(),5.0);
+        Point p1 = new Point (5.0, 5.0);
+        Point p2 = new Point (2.0, 1.0);
+        Assert.assertEquals(p1.distance(p2),5.0);
     }
     @Test
     public void DistTest2() {
-        Point d = new Point (2.0, 5.0, 5.0, 1.0);
-        Assert.assertEquals(d.distance(),5.0);
+        Point p1 = new Point (6.0, 5.0);
+        Point p2 = new Point (2.0, 5.0);
+        Assert.assertEquals(p1.distance(p2),4.0);
     }
     @Test
     public void DistTest3() {
-        Point d = new Point (7.0, 5.0, 2.0, 0.0);
-        Assert.assertEquals(d.distance(),4.0);
+        Point p1 = new Point (5.0, 5.0);
+        Point p2 = new Point (2.0, 3.0);
+        Assert.assertEquals(p1.distance(p2),4.0);
     }
     @Test
     public void DistTest4() {
-        Point d = new Point (15.0, 5.0, 2.0, 1.0);
-        Assert.assertEquals(d.distance(),6.0);
+        Point p1 = new Point (5.0, 5.0);
+        Point p2 = new Point (7.0, 4.0);
+        Assert.assertEquals(p1.distance(p2),6.0);
     }
 
 
