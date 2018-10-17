785479d16e7e4a8a47c24faab2e0eab085819417
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-17 11:51:42 +0500

    ПДТ-57 Вычисление расстояния через функцию в запускаемом классе

--- sandbox/src/main/java/ru/mytest/functions/CalculationDist.java
+++ sandbox/src/main/java/ru/mytest/functions/CalculationDist.java
@@ -1,19 +1,16 @@
 package ru.mytest.functions;
 
-public class Calculation {
+public class CalculationDist {
     public static void main (String[] args) {
- /*       Point p1 = new Point();
-        p1.x = 5.0;
-        p1.y = 5.0;
+        Point p1 = new Point();
         Point p2 = new Point();
+        p1.x = 7.0;
+        p1.y = 5.0;
         p2.x = 2.0;
-        p2.y = 1.0;*/
-        Point p1 = new Point(7.0, 5.0);
-        Point p2 = new Point(2.0, 3.0);
+        p2.y = 3.0;
         System.out.println("Расстояние между точками с координатами " + "(" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно = " + distance(p1, p2));
 
     }
-
     public static double distance(Point p1, Point p2) {
         return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
     }
