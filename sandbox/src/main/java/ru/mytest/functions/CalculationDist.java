6dd113ae0fce4803fe30635696f839259349727e
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-17 13:46:16 +0500

    ПДТ-57 Вычисление расстояния при помощи метода в классе Point

--- sandbox/src/main/java/ru/mytest/functions/CalculationDist.java
+++ sandbox/src/main/java/ru/mytest/functions/CalculationDist.java
@@ -2,12 +2,10 @@ package ru.mytest.functions;
 
 public class CalculationDist {
     public static void main (String[] args) {
-        Point p1 = new Point(5.0, 5.0);
-        Point p2 = new Point(2.0, 1.0);
-        System.out.println("Расстояние между точками с координатами " + "(" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно = " + distance(p1, p2));
+        Point p1 = new Point(6.0, 5.0);
+        Point p2 = new Point(2.0, 5.0);
+        double d = p1.distance(p2);
+        System.out.println("Расстояние между точками с координатами " + "(" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно = " + d);
+    }
 
-    }
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
-    }
 }
