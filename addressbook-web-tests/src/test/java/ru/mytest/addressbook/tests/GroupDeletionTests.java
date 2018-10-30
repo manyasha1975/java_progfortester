39a7d614b40ad091a826dd7f05b6a5c987aa60f3
Maria Hudyakova <manyasha1975@rambler.ru>
2018-10-30 19:32:45 +0500

    Реализовано обеспечение выполнения предусловий для групп

--- addressbook-web-tests/src/test/java/ru/mytest/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/mytest/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.mytest.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.mytest.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Group3", "Group_new", "Group_new1"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/ru/mytest/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/mytest/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Group3", "Group_new", "Group_new1"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Group_3", "Test group3", "Test group3_2"));
