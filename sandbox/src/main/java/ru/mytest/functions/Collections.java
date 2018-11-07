f6b1cb82db5ba4046c0551317d1c6c6c3fb5355d
Maria Hudyakova <manyasha1975@rambler.ru>
2018-11-07 19:17:54 +0500

    Добавлен класс Collections
    Примеры работы со списками List

---
+++ sandbox/src/main/java/ru/mytest/functions/Collections.java
@@ -0,0 +1,22 @@
+package ru.mytest.functions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+  public static void main (String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+    
+    for (int i = 0; i < langs.length; i++) {
+      System.out.println("Я хочу выучить " + langs[i]);
+    }
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+}
