1909e4558175e06337de3409ce62705d03e89018
Maria Hudyakova <manyasha1975@rambler.ru>
2018-11-20 18:23:22 +0500

    Группы. Реализована передача параметров для генератора в командной строке
    Использование библиотеки jCommander

--- addressbook-web-tests/src/test/java/ru/mytest/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/mytest/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.mytest.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.mytest.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,16 +14,31 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
   public static void main(String[] args) throws IOException {
     //we get two parameters - count of group and path to file in which data to generate
-    int count = Integer.parseInt(args[0]); //convert to integer
-    File file = new File(args[1]);
-
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator); //jcommander is library for work with command line
+    try {
+      jCommander.parse(args);  // get some args from command line
+    } catch (ParameterException ex) {
+      jCommander.usage(); //if parameters are incorrect, jcommander send how to use this GroupDataGenerator program
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file); //open file to write
     for (GroupData group : groups) {
@@ -29,7 +47,7 @@ public class GroupDataGenerator {
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withName(String.format("group %s", i))
