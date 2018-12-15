90e7c5128d919f3c142acb2f833e8401435d13a4
Maria Hudyakova <manyasha1975@rambler.ru>
2018-12-15 22:12:33 +0500

    Реализовано получение почты на встроенный в тесты почтовый сервер
    Реализована регистрация пользователя

--- mantis-tests/src/test/java/ru/mytest/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/mytest/mantis/appmanager/RegistrationHelper.java
@@ -12,15 +12,14 @@ public class RegistrationHelper extends HelperBase {
     driver.get(app.getProperty("web.baseUrl") + "signup_page.php");
     type(By.name("username"), username);
     type(By.name("email"), email);
-    //click(By.cssSelector("input[type='submit']"));
     click(By.cssSelector("input[value='Signup']"));
+    //click(By.cssSelector("input[type='submit']"));
   }
 
   public void finish(String confirmationLink, String password) {
     driver.get(confirmationLink);
     type(By.name("password"), password);
     type(By.name("password_confirm"), password);
-    //click(By.cssSelector("button[value='Update User']"));
-    click(By.cssSelector("button[type='submit']"));
+    submit(By.cssSelector("button[type='submit']"));
   }
 }
