946d38687f762e0079699f484604a02c111703da
Maria Hudyakova <manyasha1975@rambler.ru>
2018-12-19 14:55:13 +0500

    Организован запуск тестов на сервере непрерывной интеграции

--- addressbook-web-tests/src/test/java/ru/mytest/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/mytest/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.mytest.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -53,6 +54,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
       driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     baseUrl = "https://www.katalon.com/";
